fa5c88a86ed4e0089f643ca6c756c0fc513c6a2c
shamsisadr01 <shamsisadr01@gmail.com>
2025-04-11 22:35:25 +0330

    Config Redis

---
+++ Services/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
